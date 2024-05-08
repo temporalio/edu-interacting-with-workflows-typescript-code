8b36d8b5a4461d510fb8783c2d10a743712a1097
angelazhou32 <angela.zhou@temporal.io>
2024-05-08 12:51:35 -0400

    remove answers from practice dir

--- exercises/async-activity-completion/practice/src/activities.ts
+++ exercises/async-activity-completion/practice/src/activities.ts
@@ -6,14 +6,14 @@ import { TranslationActivityInput, TranslationActivityOutput } from './shared';
 export async function translateTerm(input: TranslationActivityInput): Promise<TranslationActivityOutput> {
   // TODO Part A: Extract the task token and replace line 9 with it.
   // This can be done taking the `activityInfo` method below and chaining `taskToken` to it.
-  const taskToken = activityInfo().taskToken;
+  const taskToken = '';
 
   log.info('Translating term:', { LanguageCode: input.languageCode, Term: input.term });
 
   // TODO PART B: Use setTimeout to asynchronously call `verifyAndCompleteTranslation`
   // after a one second delay.
   // verifyAndCompleteTranslation should take in task token from part A and the input passed in translateTerm.
-  setTimeout(() => verifyAndCompleteTranslation(taskToken, input), 1000);
+  setTimeout(() => verifyAndCompleteTranslation('', ''), 1000);
 
   //TODO Part C: Replace the below line to throw CompleteAsyncError.
   throw new Error();
